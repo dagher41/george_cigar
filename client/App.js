87d57263ea1bd6fd6008ec630668a6589786630b
George Dagher <dagher.george41@gmail.com>
2019-03-11 16:30:11 -0400

    refactored product list pages into a single component

--- client/App.js
+++ client/App.js
@@ -3,12 +3,9 @@ import { Switch, Route } from 'react-router-dom';
 import Header from './modules/_common/components/Header';
 import Footer from './modules/_common/components/Footer';
 import HomePage from './modules/home/HomePage';
-import CigarsPage from './modules/cigars/CigarsPage';
-import VapesPage from './modules/vapes/VapesPage';
-import GlassPage from './modules/glass/GlassPage';
-import CBDPage from './modules/cbd/CBDPage';
 import ContactUsPage from './modules/contact-us/ContactUsPage';
 import ScrollTop from './modules/_common/components/ScrollTop';
+import ProductListPage from './modules/_common/components/ProductListPage';
 
 export class App extends Component {
     render() {
@@ -17,10 +14,42 @@ export class App extends Component {
                 <Header />
                 <Switch>
                     <Route exact path="/" component={ScrollTop(HomePage)} />
-                    <Route path="/cigars" component={ScrollTop(CigarsPage)} />
-                    <Route path="/vapes" component={ScrollTop(VapesPage)} />
-                    <Route path="/glass" component={ScrollTop(GlassPage)} />
-                    <Route path="/cbd" component={ScrollTop(CBDPage)} />
+                    <Route path="/cigars"
+                        render={() => <ProductListPage
+                            productTitle="Cigars"
+                            productListTitle="Cigar"
+                            tagName="cigars"
+                            heroClassName="bg-cigars-1"
+                            heroText="Cigars, Pipes and Humidors"
+                            listClassName="bg-cigars-tiled"
+                        />} />
+                    <Route path="/vapes"
+                        render={() => <ProductListPage
+                            productTitle="Vapes"
+                            productListTitle="Vape"
+                            tagName="vapes"
+                            heroClassName="bg-vapes-1"
+                            heroText="Vapes, Juices and Accessories"
+                            listClassName="bg-vape-tiled"
+                        />} />
+                    <Route path="/glass"
+                        render={() => <ProductListPage
+                            productTitle="Glass"
+                            productListTitle="Glass"
+                            tagName="glass"
+                            heroClassName="bg-glass-1"
+                            heroText="Glass Pieces and Accessories"
+                            listClassName="bg-glass-tiled"
+                        />} />
+                    <Route path="/cbd"
+                        render={() => <ProductListPage
+                            productTitle="CBD"
+                            productListTitle="CBD"
+                            tagName="cbd"
+                            heroClassName="bg-cbd-1"
+                            heroText="Our CBD selection"
+                            listClassName="bg-cbd-tiled"
+                        />} />/>
                     <Route path="/contact-us" component={ScrollTop(ContactUsPage)} />
                 </Switch>
                 <Footer />
